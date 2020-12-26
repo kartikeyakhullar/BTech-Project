f0ff4512a9bf82741111eeeec9363d72c4e8a743
Kartikeya <kartikeya.knightriders@gmail.com>
2020-12-26 22:04:43 +0530

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import Home from './pages/Home';
+import Report from './pages/Report';
+import Team from './pages/Team';
+import Presentation from './pages/Presentation';
+import Dataset from './pages/Dataset';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/report" component={Report} />
+          <Route path="/team" component={Team} />
+          <Route path="/presentation" component={Presentation} />
+          <Route path="/dataset" component={Dataset} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
