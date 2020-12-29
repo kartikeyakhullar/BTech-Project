43fff8eb955fcfebda7a557140df4915cd08e6a5
Kartikeya <kartikeya.knightriders@gmail.com>
2020-12-29 22:06:06 +0530

    Final from Kartikeya

--- src/pages/List.js
+++ src/pages/List.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 
 function List({keywords}) {
+    // 0-99
+    const idx1 = Math.floor(Math.random() * 100); 
+    // 100-199
+    const idx2 = Math.floor(Math.random() * 100) + 100;
+    // 200-299
+    const idx3 = Math.floor(Math.random() * 100) + 200;
     return (
         <>
             <h5> Keywords searched : </h5>
             {keywords.map((word)=>{
                 return  word 
             })}
+            <ul>
+                <li> {} </li>
+                <li> {} </li>
+                <li> {} </li>
+            </ul>
         </>
     )
 }
